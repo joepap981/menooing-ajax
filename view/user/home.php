9ae0a55b159f41b1a4489df816d2d8f9e7b90957
joepap981 <joepap981@gmail.com>
2018-02-05 14:33:48 -0600

    Align topnav with sidenav

--- view/user/home.php
+++ view/user/home.php
@@ -2,37 +2,10 @@
 <link href="css/user/userlogin.css" rel="stylesheet">
 <link href="css/user/home.css" rel="stylesheet">
 
-<!-- Sidebar -->
-<div class="row">
-  <div class="col-md-3" role="banner">
-    <div id="sidebar-wrapper">
-      <ul class="sidebar-nav">
-        <li>
-          <a href="#">Dashboard</a>
-        </li>
-        <li>
-          <a href="#">Shortcuts</a>
-        </li>
-        <li>
-          <a href="#">Overview</a>
-        </li>
-        <li>
-          <a href="#">Events</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
 
-  <div class="col-md-9">
+
+<!--Content -->
+  <div class="container home-content">
     <h1> Hey jude </h1>
   </div>
 </div>
--- view/user/top-nav-default.php
+++ view/user/top-nav-default.php
@@ -1,31 +1,57 @@
 <!-- Navigation -->
 <nav class="navbar navbar-light bg-light static-top">
-  <div class="container">
-    <span ng-click ="redirect('/')" class="navbar-brand logo"> menooing </span>
-    <!--show before a user is in session -->
-    <div id="guest" ng-if="session['user_id'] == null" >
-      <div class="btns">
-        <span ng-hide="isURL('/sharekitchen')" class="clear-btn larger-space" ng-click="redirect('/sharekitchen')"> Share Your Kitchen </span>
-        <span class="clear-btn" ng-click="redirect('/signin')"> Sign in</span>
-        <span class="clear-btn" ng-click="redirect('/signup')">Sign up</span>
-      </div>
+  <!-- Sidebar -->
+  <div class="navbar-default">
+    <div>
+      <ul class="sidebar-nav">
+        <li>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <a href="#">Shortcuts</a>
+        </li>
+        <li>
+          <a href="#">Overview</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </div>
-    <!-- show when a session is running-->
-    <div id="logon-user" ng-if="session['user_id'] != null" >
-      <div id="session-user-name"> {{ session['user_first_name'] }} </div>
-        <!-- User ICON NAVIGATION -->
-      <span class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img class="img-fluid rounded-circle user_img" src="/img/default-user.png" alt="">
-        </a>
-        <span class="dropdown-menu" aria-labelledby="messagesDropdown">
-          <a class="dropdown-item" ng-click="redirect('/home')">Profile</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Settings</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" ng-click="logout()">Logout</a>
-        </span>
+  </div>
+  <div ng-click ="redirect('/')" class="navbar-brand cursor-pointer"> menooing </div>
+  <!--show before a user is in session -->
+  <div id="guest" ng-if="session['user_id'] == null" >
+    <div class="btns">
+      <span ng-hide="isURL('/sharekitchen')" class="clear-btn larger-space" ng-click="redirect('/sharekitchen')"> Share Your Kitchen </span>
+      <span class="clear-btn" ng-click="redirect('/signin')"> Sign in</span>
+      <span class="clear-btn" ng-click="redirect('/signup')">Sign up</span>
+    </div>
+  </div>
+  <!-- show when a session is running-->
+  <div id="logon-user" ng-if="session['user_id'] != null" >
+    <div id="session-user-name"> {{ session['user_first_name'] }} </div>
+      <!-- User ICON NAVIGATION -->
+    <span class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <img class="img-fluid rounded-circle user_img" src="/img/default-user.png" alt="">
+      </a>
+      <span class="dropdown-menu" aria-labelledby="messagesDropdown">
+        <a class="dropdown-item" ng-click="redirect('/home')">Profile</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Settings</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" ng-click="logout()">Logout</a>
       </span>
-    </div>
+    </span>
   </div>
 </nav>
