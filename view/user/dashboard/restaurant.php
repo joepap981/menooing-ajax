1b7e415015a2698ddfa57319bd62c98e76a79d38
joepap981 <joepap981@gmail.com>
2018-02-08 15:57:28 -0600

    Implement listing user restaurants

--- view/user/dashboard/restaurant.php
+++ view/user/dashboard/restaurant.php
@@ -3,49 +3,15 @@
 </div>
 
 <div id="restaurant-list">
-  <div class="card">
+  <div class="card" ng-repeat= "restaurant in userRestaurants">
     <div class="row">
       <div class="col-md-3">
         <img class="card-img-top" src="http://placehold.it/500x325" alt="">
       </div>
       <div class="col-md-9">
         <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-        </div>
-        <div class="card-footer">
-          <a href="#" class="btn btn-primary">Find Out More!</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="row">
-      <div class="col-md-3">
-        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-      </div>
-      <div class="col-md-9">
-        <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-        </div>
-        <div class="card-footer">
-          <a href="#" class="btn btn-primary">Find Out More!</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="row">
-      <div class="col-md-3">
-        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-      </div>
-      <div class="col-md-9">
-        <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          <h4 class="card-title">{{ restaurant.restaurant_name }}</h4>
+          <p class="card-text"> {{restaurant.restaurant_address1}} {{ restaurant.restaurant_state }} {{restaurant.restaurant_zip }}</p>
         </div>
         <div class="card-footer">
           <a href="#" class="btn btn-primary">Find Out More!</a>
