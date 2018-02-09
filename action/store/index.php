fbf4d18e2c19d64cd701e95d27248e6cd9615918
joepap981 <joepap981@gmail.com>
2018-02-09 10:21:06 -0600

    Delete files

--- action/store/index.php
+++
@@ -1,1 +0,0 @@
-{"systemTiemStr":"2017-12-14 11:52:35","data":[{"create_date":null,"itemSysNo":"70","itemNo":"ST001","storeName":"VR A"},{"create_date":null,"itemSysNo":"72","itemNo":"ST001","storeName":"VR B"}]}
