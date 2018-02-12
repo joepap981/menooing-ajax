67efb5734f1e1e2c78d4f5ced5bf8a63ef1fe2dd
joepap981 <joepap981@gmail.com>
2018-02-12 13:12:48 -0600

    Set restaurant profile form

--- view/user/dashboard/restaurant/restaurant-profile.php
+++ view/user/dashboard/restaurant/restaurant-profile.php
@@ -103,29 +103,109 @@
                   <div class="col-sm-2">
                     <label for="restaurantCity"> Open  </label>
                   </div>
-                  <div class="col-sm-4">
+                  <div class="col-sm-2">
                     <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
                   </div>
                   <span> to </span>
-                  <div class="col-sm-4">
+                  <div class="col-sm-2">
+                    <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
+                  </div>
+
+                  <div class="col-sm-1">
+                    <input type="text" readonly class="form-control-plaintext" placeholder="">
+                  </div>
+
+                  <div class="col-sm-2">
+                    <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
+                  </div>
+                  <span> to </span>
+                  <div class="col-sm-2">
                     <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
                   </div>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col-sm-3">
-                  <label for="restaurantPhone"> Phone: </label>
+                <div class="col-sm-4">
+                  <label for="restaurantCuisine"> Restaurant Keyword </label>
                 </div>
-                <div class="col-sm-9">
-                  <input type="text" class="form-control" id="restaurantPhone"  placeholder="" >
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="restaurantKeyword"  placeholder="" >
                 </div>
               </div>
+
+              <div class="form-group row">
+                <div class="col-sm-4">
+                  <label for="restaurantPrice"> Price Range </label>
+                </div>
+                <div class="col-sm-8">
+                  <input type="text" readonly class="form-control-plaintext" id="restaurantPrice"  placeholder="" >
+                </div>
+              </div>
+
             </div>
             </form>
           </div>
-        </div>
 
+
+
+          <div class="card restaurant-profile">
+            <div class="card-header"> Sharee Info </div>
+            <div class="card-block container">
+
+              <form>
+
+                <div class="row">
+                  <div class="form-group row col">
+                    <div class="col-sm-2">
+                      <label for="restaurantCity"> Open  </label>
+                    </div>
+                    <div class="col-sm-2">
+                      <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
+                    </div>
+                    <span> to </span>
+                    <div class="col-sm-2">
+                      <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
+                    </div>
+
+                    <div class="col-sm-1">
+                      <input type="text" readonly class="form-control-plaintext" placeholder="">
+                    </div>
+
+                    <div class="col-sm-2">
+                      <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
+                    </div>
+                    <span> to </span>
+                    <div class="col-sm-2">
+                      <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <div class="col-sm-4">
+                    <label for="restaurantCuisine"> Restaurant Keyword </label>
+                  </div>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="restaurantKeyword"  placeholder="" >
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <div class="col-sm-4">
+                    <label for="restaurantPrice"> Price Range </label>
+                  </div>
+                  <div class="col-sm-8">
+                    <input type="text" readonly class="form-control-plaintext" id="restaurantPrice"  placeholder="" >
+                  </div>
+                </div>
+
+              </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
