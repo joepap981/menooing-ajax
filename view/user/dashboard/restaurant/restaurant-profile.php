4cb856053883405611d338d85857be911aafdfa7
joepap981 <joepap981@gmail.com>
2018-02-13 21:56:53 -0600

    Make ui button for saving changes to restaurant profile

--- view/user/dashboard/restaurant/restaurant-profile.php
+++ view/user/dashboard/restaurant/restaurant-profile.php
@@ -15,6 +15,14 @@
         <div class="btn clear-btn" ng-click="redirect('/restaurant-menu')"> Menu </div>
       </div>
 
+      <div class="card restaurant-profile">
+        <div class="card-header"> Basic Info </div>
+        <div class="card-block container">
+          <a href="#"><img id="restaurant-image" src="http://via.placeholder.com/700x300" alt=""></a>
+          <a href="#"> Add image </a>
+        </div>
+      </div>
+
 
       <div class="card restaurant-profile">
         <div class="card-header"> Basic Info </div>
@@ -35,7 +43,7 @@
                 <label for="restaurantAdd1"> Address 1: </label>
               </div>
               <div class="col-sm-9">
-                <input type="text" class="form-control" id="restaurantAdd1"  placeholder="" text="Sushiyaa">
+                <input type="text" class="form-control" id="restaurantAdd1"  ng-model ="restaurant.restaurant_address1" text="Sushiyaa">
               </div>
             </div>
 
@@ -44,36 +52,36 @@
                 <label for="restaurantAdd2"> Address 2: </label>
               </div>
               <div class="col-sm-9">
-                <input type="text" class="form-control" id="restaurantAdd2" placeholder="" >
+                <input type="text" class="form-control" id="restaurantAdd2"  ng-model ="restaurant.restaurant_address2">
               </div>
             </div>
 
             <div class="row">
-              <div class="form-group row col">
+              <div class="form-group row col-md-6">
                 <div class="col-sm-4">
                   <label for="restaurantCity"> City: </label>
                 </div>
                 <div class="col-sm-8">
-                  <input type="text" class="form-control" id="restaurantCity"  placeholder="">
+                  <input type="text" class="form-control" id="restaurantCity"  ng-model ="restaurant.restaurant_city">
                 </div>
               </div>
 
-              <div class="form-group row col">
+              <div class="form-group row col-md-3">
                 <div class="col-sm-4">
                   <label for="restaurantState"> State: </label>
                 </div>
                 <div class="col-sm-8">
-                  <input type="text" class="form-control" id="restaurantState"  placeholder="" >
+                  <input type="text" class="form-control" id="restaurantState" ng-model ="restaurant.restaurant_state" >
                 </div>
               </div>
 
 
-              <div class="form-group row col">
-                <div class="col-sm-4">
+              <div class="form-group row col-md-4">
+                <div class="col-sm-5">
                   <label for="restaurantZip"> Zipcode: </label>
                 </div>
-                <div class="col-sm-8">
-                  <input type="text" class="form-control" id="restaurantZip"  placeholder="" >
+                <div class="col-sm-7">
+                  <input type="text" class="form-control" id="restaurantZip" ng-model ="restaurant.restaurant_zip" >
                 </div>
               </div>
             </div>
@@ -83,17 +91,17 @@
                 <label for="restaurantPhone"> Phone: </label>
               </div>
               <div class="col-sm-9">
-                <input type="text" class="form-control" id="restaurantPhone"  placeholder="" >
+                <input type="text" class="form-control" id="restaurantPhone"  ng-model ="restaurant.restaurant_phone" >
               </div>
             </div>
           </div>
+
           </form>
         </div>
 
         <div class="card restaurant-profile">
           <div class="card-header"> Restaurant Management </div>
           <div class="card-block container">
-
             <form>
 
               <div class="row">
@@ -102,23 +110,23 @@
                     <label for="restaurantCity"> Open  </label>
                   </div>
                   <div class="col-sm-2">
-                    <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
+                    <input type="text" class="form-control" id="restaurantOpenDay"  ng-model ="restaurant.restaurant_open_day">
                   </div>
                   <span> to </span>
                   <div class="col-sm-2">
-                    <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
+                    <input type="text" class="form-control" id="restaurantCloseDay" ng-model ="restaurant.restaurant_close_day">
                   </div>
 
                   <div class="col-sm-1">
-                    <input type="text" readonly class="form-control-plaintext" placeholder="">
+                    <input type="text" readonly class="form-control-plaintext">
                   </div>
 
                   <div class="col-sm-2">
-                    <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
+                    <input type="text" class="form-control" id="restaurantOpenHour" ng-model ="restaurant.restaurant_open_hour">
                   </div>
                   <span> to </span>
                   <div class="col-sm-2">
-                    <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
+                    <input type="text" class="form-control" id="restaurantCloseHour" ng-model ="restaurant.restaurant_close_hour">
                   </div>
                 </div>
               </div>
@@ -128,7 +136,7 @@
                   <label for="restaurantCuisine"> Restaurant Keyword </label>
                 </div>
                 <div class="col-sm-8">
-                  <input type="text" class="form-control" id="restaurantKeyword"  placeholder="" >
+                  <input type="text" class="form-control" id="restaurantKeyword" ng-model ="restaurant.restaurant_cuisine" >
                 </div>
               </div>
 
@@ -142,67 +150,10 @@
               </div>
 
             </div>
+
             </form>
           </div>
-
-
-
-          <div class="card restaurant-profile">
-            <div class="card-header"> Sharee Info </div>
-            <div class="card-block container">
-
-              <form>
-
-                <div class="row">
-                  <div class="form-group row col">
-                    <div class="col-sm-2">
-                      <label for="restaurantCity"> Open  </label>
-                    </div>
-                    <div class="col-sm-2">
-                      <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
-                    </div>
-                    <span> to </span>
-                    <div class="col-sm-2">
-                      <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
-                    </div>
-
-                    <div class="col-sm-1">
-                      <input type="text" readonly class="form-control-plaintext" placeholder="">
-                    </div>
-
-                    <div class="col-sm-2">
-                      <input type="text" class="form-control" id="restaurantOpenDay"  placeholder="">
-                    </div>
-                    <span> to </span>
-                    <div class="col-sm-2">
-                      <input type="text" class="form-control" id="restaurantCloseDay"  placeholder="">
-                    </div>
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <div class="col-sm-4">
-                    <label for="restaurantCuisine"> Restaurant Keyword </label>
-                  </div>
-                  <div class="col-sm-8">
-                    <input type="text" class="form-control" id="restaurantKeyword"  placeholder="" >
-                  </div>
-                </div>
-
-                <div class="form-group row">
-                  <div class="col-sm-4">
-                    <label for="restaurantPrice"> Price Range </label>
-                  </div>
-                  <div class="col-sm-8">
-                    <input type="text" readonly class="form-control-plaintext" id="restaurantPrice"  placeholder="" >
-                  </div>
-                </div>
-
-              </div>
-              </form>
-            </div>
-          </div>
-
+          <button id="restaurant-save-changes" class="btn btn-primary"> Save Changes </button>
         </div>
       </div>
     </div>
