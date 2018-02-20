30d5752196c809756792b4227271bb0427ce4c74
joepap981 <joepap981@gmail.com>
2018-02-20 11:22:17 -0600

    no message

--- view/user/dashboard/restaurant/restaurant-new-sharer4.php
+++ view/user/dashboard/restaurant/restaurant-new-sharer4.php
@@ -7,22 +7,72 @@
       <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
   </div>
   <div class="restaurant-text-title">
-    <h3> Add a logo or image to represent your restaurant  </h3>
-    <small> You can skip this step and add an image later! </small>
+    <h3> Now, we need some information for you to become a Sharer!  </h3>
+    <p> We need to know a few things about you! </p>
   </div>
   <div class="margin-auto">
     <div class="restaurant-information-form">
-      <div id="restaurantAdd" class="card">
-         <img class="card-img-top" src="http://via.placeholder.com/250x150" alt="Card image cap">
-         <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <form name="restaurantRegistration" class="restaurant-information-form">
+        <div class="form-group row">
+          <label class="col-label" for="owner_name">Name</label>
+          <input type="text" class="form-control col-input" placeholder="" required>
         </div>
-      </div>
+        <div class="form-group row">
+          <label class="col-label" for="owner_phone">Mobile</label>
+          <input type="text" class="form-control col-input" placeholder="" required>
+        </div>
+        <div class="form-group row">
+          <span>Food Mananger/Handler Certificate</span>
+        </div>
+          <input type="file" placeholder="" required>
 
-      <div id="restaurantAdd" class="btn-toolbar">
-        <button ng-click="redirect('/restaurant-new-sharer3')" class="btn btn-secondary"> Back </button>
-        <button ng-click="saveAndContinue('/restaurant-new-sharer5')" class="btn btn-primary"> Continue </button>
-      </div>
+
+
+        <div class="restaurant-text-title">
+          <p> What are the operation hours? </p>
+        </div>
+
+        <div class="form-group row">
+          <span class="col-label">Open Days</span>
+          <div class="col-input">
+            <select class="custom-select" id="openDay">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+            <span> to </span>
+            <select class="custom-select" id="closeDay">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <span class="col-label">Open Hours</span>
+          <div class="col-input">
+            <select class="custom-select" id="openHour">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+            <span> to </span>
+            <select class="custom-select" id="closeHour">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
+        <div id="restaurantAdd" class="btn-toolbar mt-3">
+          <button ng-click="redirect('/restaurant-new-sharer')" class="btn btn-secondary"> Back </button>
+          <button ng-click="saveAndContinue('/restaurant-new-sharer3')" class="btn btn-primary"> Continue </button>
+        </div>
+      </form>
     </div>
   </div>
 
