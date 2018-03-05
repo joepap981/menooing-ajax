db44370e9f46d7fc94872d3560a69ece2835d2bc
joepap981 <joepap981@gmail.com>
2018-03-06 02:55:55 +0900

    Make restaurant profile address edit Google autocomplete

--- view/user/dashboard/restaurant/restaurant-profile.php
+++ view/user/dashboard/restaurant/restaurant-profile.php
@@ -29,24 +29,25 @@
                   <input type="text" readonly class="form-control-plaintext col-input" id="restaurantName" ng-model ="restaurant.restaurant_name" value="{{ restaurant.restaurant_name }}">
                 </div>
 
-                <div class="form-group row">
-                  <span class="col-label" for="restaurantAdd1"> Address 1 </span>
-                  <input class="form-control col-input" type="text" id="restaurantAdd1"  ng-model ="restaurant.restaurant_address1">
+                <div class=" form-group row" >
+                  <span class="col-label" for="restaurantName"> Address: </span>
+                  <div class="col-input">
+                    <input data-toggle="collapse" data-target="#collapseAddress" type="text" readonly class="form-control-plaintext" id="restaurantAddress" ng-model ="restaurant.restaurant_name" value="click to add address">
+                    <div class="collapse" id="collapseAddress">
+                      <div class="card card-body collapse">
+                        <div id="locationField">
+                          <input ng-controller= "googlePlaceCtrl" id="autocomplete" placeholder="Enter your address" ng-focus="geolocate()" type="text"></input>
+                        </div>
+                        <div class="btn-box">
+                          <button data-toggle= "collapse" data-target="#collapseAddress" class="btn secondary"> Cancel </button>
+                          <button class="btn btn-primary"> Save Changes </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
-                <div class="form-group row">
-                  <span class="col-label" for="restaurantAdd2"> Address 2 </span>
-                  <input type="text" class="form-control col-input" id="restaurantAdd2"  ng-model ="restaurant.restaurant_address2">
-                </div>
 
-                <div class="form-group row">
-                  <span id="city-label" for="restaurantCity"> City </span>
-                  <input id="city-input" type="text" class="form-control" id="restaurantCity"  ng-model ="restaurant.restaurant_city">
-                  <span id="state-label" for="restaurantState"> State </span>
-                  <input id="state-input" type="text" class="form-control" id="restaurantState" ng-model ="restaurant.restaurant_state">
-                  <span id="zip-label" for="restaurantZip"> Zipcode </span>
-                  <input id="zip-input" type="text" class="form-control" id="restaurantZip" ng-model ="restaurant.restaurant_zip">
-                </div>
 
                 <div class="form-group row">
                   <span class="col-label" for="restaurantPhone"> Phone: </span>
