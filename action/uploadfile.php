2637c1df98d18e57227a410ddbb727507024da61
joepap981 <joepap981@gmail.com>
2018-03-17 04:46:58 +0900

    Clean up uploadfile.php

--- action/uploadfile.php
+++ action/uploadfile.php
@@ -13,11 +13,10 @@ $select_query = "SELECT user_storage_salt FROM tb_user_info WHERE user_ref = " .
 $select_result = mysqli_fetch_array(mysqli_query($conn, $select_query));
 
 //storage salt for slightly more secure user document storage file name
-$storage_salt = substr(sha1(rand()), 0, 10);
-
-$filename = $storage_salt;
+$filename = substr(sha1(rand()), 0, 10);
 $upload_location = "../noexec/" . $user['user_id'] . "/" . $select_result['user_storage_salt'] . "/";
 
+//upload file to the directory of user
 if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_location.$filename)) {
   $insert_query = "UPDATE $table_name SET user_cert_img_ref = '$upload_location$filename';";
   $insert_result = mysqli_query($conn, $insert_query);
