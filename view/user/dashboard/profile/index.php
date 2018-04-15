b615653d4383f4ce49e9c3c174cd837349b1b75c
joepap981 <joepap981@gmail.com>
2018-04-16 03:43:05 +0900

    Finish UI for user profile

--- view/user/dashboard/profile/index.php
+++ view/user/dashboard/profile/index.php
@@ -43,206 +43,62 @@
           <div class="card restaurant-profile">
             <div class="card-header"> User Basic Information </div>
             <div class="card-block container">
-              <div class="row">
-                
+              <div class="form-group row">
+                <label class="col-label"> First Name: </label>
+                <input class="col-input form-control"> </input>
+              </div>
+              <div class="form-group row">
+                <label class="col-label"> Last Name: </label>
+                <input class="col-input form-control"> </input>
+              </div>
+              <div class="form-group row">
+                <label class="col-label"> Phone: </label>
+                <input class="col-input form-control"> </input>
               </div>
 
+              <button class="btn btn-primary mt-3 pull-right"> Save Changes </button>
 
-              <form>
-                <!-- Restaurant hours dropdown-->
-                <div class="form-group row">
-                  <span class="col-label"> Restaurant Hours </span>
-                  <div class="col-input">
-                    <p ng-if ="checkDateHours() == true" data-toggle="collapse" data-target="#collapseOpen" class="pointer" id="restaurantOpen"> {{ restaurant.open_time }}</p>
-
-                    <div class="collapse" id="collapseOpen">
-                      <div class="card card-body collapse">
-                        <div class="form-group row">
-                          <span class="w-25">Open Days</span>
-                          <div class="w-75">
-                            <select class="custom-select" id="openDay" ng-model='restaurant.open_day'>
-                              <option value="Sunday" selected>Sunday</option>
-                              <option value="Monday">Monday</option>
-                              <option value="Tuesday">Tuesday</option>
-                              <option value="Wednesday">Wednesday</option>
-                              <option value="Thursday">Thursday</option>
-                              <option value=">Friday">Friday</option>
-                              <option value=">Saturday">Saturday</option>
-                            </select>
-                            <span> to </span>
-                            <select class="custom-select" id="closeDay" ng-model='restaurant.close_day'>
-                              <option value="Sunday" selected>Sunday</option>
-                              <option value="Monday">Monday</option>
-                              <option value="Tuesday">Tuesday</option>
-                              <option value="Wednesday">Wednesday</option>
-                              <option value="Thursday">Thursday</option>
-                              <option value="Friday">Friday</option>
-                              <option value="Saturday">Saturday</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div class="form-group row">
-                          <span class="w-25">Open Hours</span>
-                          <div class="row w-75 shiftright">
-                            <div class="row half">
-                              <select class="custom-select col timepick" id="openHour" ng-model = "operationHours.openHour">
-                                <option value="00" selected>00</option>
-                                <option value="01">01</option>
-                                <option value="02">02</option>
-                                <option value="03">03</option>
-                                <option value="04">04</option>
-                                <option value="05">05</option>
-                                <option value="06">06</option>
-                                <option value="07">07</option>
-                                <option value="08">08</option>
-                                <option value="09">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                              </select>
-                              <span> : </span>
-                              <select class="custom-select col timepick" id="openMin" ng-model = "operationHours.openMin">
-                                <option value="00" selected>00</option>
-                                <option value="30">30</option>
-                              </select>
-                              <select class="custom-select col timepick" id="open" ng-model = "operationHours.open">
-                                <option value="am" selected>am</option>
-                                <option value="pm">pm</option>
-                              </select>
-                            </div>
-                            <div class="row half">
-                              <select class="custom-select col timepick" id="closeHour" ng-model = "operationHours.closeHour">
-                                <option value="00" selected>00</option>
-                                <option value="01">01</option>
-                                <option value="02">02</option>
-                                <option value="03">03</option>
-                                <option value="04">04</option>
-                                <option value="05">05</option>
-                                <option value="06">06</option>
-                                <option value="07">07</option>
-                                <option value="08">08</option>
-                                <option value="09">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                              </select>
-                              <span>:</span>
-                              <select class="custom-select col timepick" id="closeHour" ng-model = "operationHours.closeMin">
-                                <option value="00" selected>00</option>
-                                <option value="30">30</option>
-                              </select>
-                              <select class="custom-select col timepick" id="close" ng-model = "operationHours.close">
-                                <option value="am" selected>am</option>
-                                <option value="pm">pm</option>
-                              </select>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+            </div>
+            <!--//user basic information end-->
+          </div>
+          <div class="card restaurant-profile">
+            <div class="card-header"> User Documents </div>
+            <div class="card-block container">
+              <div class="form-group row">
+                <label class="w-25 mr-auto"> Social Security: </label>
+                <div class="w-50 ml-3">
+                  <a href="" ng-click="downloadRestaurantCert()"> Uploaded file in database </a>
+                  <span class="ml-5 pointer" data-toggle="collapse" data-target="#collapseSSN"> Change file </span>
+                  <div class="collapse mt-3 mb-3" id="collapseSSN">
+                    <div class="card card-body collapse">
+                      <input type="file" class="col-input" file-input ="restaurant_cert" ng-model ="files.restaurant_cert">
                       <div class="btn-box">
-                        <button data-toggle= "collapse" data-target="#collapseHours" class="btn secondary"> Cancel </button>
-                        <button class="btn btn-primary" ng-click="updateOperationHours()"> Save Changes </button>
+                        <button data-toggle= "collapse" data-target="#collapseCert" class="btn secondary"> Cancel </button>
+                        <button class="btn btn-primary" ng-click = "uploadFile()"> Upload </button>
                       </div>
                     </div>
                   </div>
                 </div>
-
-                <!--Restaurant category dropdown-->
-                <div class="form-group row">
-                  <span class="col-label"> Category </span>
-                  <div>
-                    <p data-toggle="collapse" data-target="#collapseCategory" class="pointer" id="restaurantAddress">{{ restaurant.restaurant_category }} </p>
-
-                    <div class="collapse" id="collapseCategory">
-                      <div class="card card-body collapse">
-                        <select id="restaurant-type" class="restaurant-hours custom-select mr-sm-2" ng-model = "input.category">
-                          <option value="Casual Dining" selected>Casual Dining</option>
-                          <option value="Fine Dining">Fine Dining</option>
-                          <option value="Fast Casual">Fast Casual</option>
-                          <option value="Fast Food">Fast Food</option>
-                        </select>
-                        <div class="btn-box">
-                          <button data-toggle= "collapse" data-target="#collapseCategory" class="btn secondary"> Cancel </button>
-                          <button ng-click= "updateCategory()" class="btn btn-primary"> Save Changes </button>
-                        </div>
+              </div>
+              <div class="form-group row">
+                <label class="w-26 mr-auto"> Food Manager Certificate: </label>
+                <div class="w-50 ml-3">
+                  <a href="" ng-click="downloadRestaurantCert()"> Uploaded file in database </a>
+                  <span class="ml-5 pointer" data-toggle="collapse" data-target="#collapseFoodManCert"> Change file </span>
+                  <div class="collapse mt-3 mb-3" id="collapseFoodManCert">
+                    <div class="card card-body collapse">
+                      <input type="file" class="col-input" file-input ="restaurant_cert" ng-model ="files.restaurant_cert">
+                      <div class="btn-box">
+                        <button data-toggle= "collapse" data-target="#collapseCert" class="btn secondary"> Cancel </button>
+                        <button class="btn btn-primary" ng-click = "uploadFile()"> Upload </button>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="form-group row">
-                  <span class="col-label"> Cuisine </span>
-                  <div>
-                    <p data-toggle="collapse" data-target="#collapseCuisine" class="pointer" id="restaurantCuisine">{{ restaurant.restaurant_cuisine }} </p>
-
-                    <div class="collapse" id="collapseCuisine">
-                      <div class="card card-body collapse">
-                        <select id="restaurant-type" ng-model = "input.cuisine" class="restaurant-hours custom-select mr-sm-2">
-                          <option value="American" selected>American</option>
-                          <option value="British">British</option>
-                          <option value="Caribbean">Caribbean</option>
-                          <option value="Chinese">Chinese</option>
-                          <option value="French">French</option>
-                          <option value="Greek">Greek</option>
-                          <option value="Indian">Indian</option>
-                          <option value="Italian">Italian</option>
-                          <option value="Japanese">Japanese</option>
-                          <option value="Mediterranean">Mediterranean</option>
-                          <option value="Mexican">Mexican</option>
-                          <option value="Moroccan">Moroccan</option>
-                          <option value="Spanish">Spanish</option>
-                          <option value="Thai">Thai</option>
-                          <option value="Turkish">Turkish</option>
-                          <option value="Korean">Korean</option>
-                        </select>
-                        <div class="btn-box">
-                          <button data-toggle= "collapse" data-target="#collapseCuisine" class="btn secondary"> Cancel </button>
-                          <button ng-click= "updateCuisine()" class="btn btn-primary"> Save Changes </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <!--Restaurant Price range-->
-                <div class="form-group row">
-                  <span class="col-label" for="restaurantPrice"> Price Range </span>
-                  <input type="text" readonly class="form-control-plaintext col-input" id="restaurantPrice"  placeholder="" >
-                </div>
-              </form>
-              <div class="row">
-                <div class="col-label">
-                  <p>Image/Logo </p>
-                </div>
-                <div class="col-input">
-                  <button type="button" class="btn btn-primary" data-toggle="modal" ng-click = "initCropper()" data-target="#addMenu">Add Image</button>
-                </div>
-                <!-- Menu Add Modal -->
-                <div class="modal fade" id="addMenu" tabindex="-1" role="dialog" aria-labelledby="addMenu" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title">Add a logo or image</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <div class=".image-container row align-items-center justify-content-center">
-                          <img id="image-holder" src="/noexec/byung1.jpg" alt="">
-                        </div>
-                        <input id="image-input" type="file" class ="menu-img mt-2" image-input ="restaurant_image"></input>
-                        <div class="btn-box mt-4 pull-right">
-                          <button type="button" class="btn btn-secondary">Cancel</button>
-                          <button type="button" class="btn btn-primary" ng-click="uploadImage()">Save Image</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                  <!--Add modal end-->
               </div>
             </div>
-            <!--//Restaurant management information end-->
+          </div>
+
         </div>
         <!--//content-box end-->
       </div>
