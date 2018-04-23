ff91d71de550d7c8946d3e8ee7837ea87d5cf792
joepap981 <joepap981@gmail.com>
2018-04-24 03:58:31 +0900

    Fix navbar and work on form UI

--- js/controller/mainCtrl.js
+++ js/controller/mainCtrl.js
@@ -1,12 +1,15 @@
-angular.module('eatersAndChefs').controller('mainCtrl',['$scope','$http','$route','$location','$element', 'formService', function ($scope, $http, $route, $location, $element, formService) {
+angular.module('eatersAndChefs').controller('mainCtrl',['$scope','$http','$route','$location', 'formService', function ($scope, $http, $route, $location, formService) {
   $scope.redirect = function(url) {
     $location.path(url);
   }
 
-
-  var url = $location.path().split('/');
-  console.log(url);
-
-
+  $scope.checkURL = function () {
+    var check = $location.path().split('/').pop();
+    if (check == "") {
+      return true;
+    } else {
+      return false;
+    };
+  }
 
 }]);
---
+++ js/controller/signUpFormCtrl.js
@@ -0,0 +1,3 @@
+angular.module('eatersAndChefs').controller('signUpFormCtrl',['$scope','$http','$route','formService', function ($scope, $http, $route, formService) {
+
+}]);
