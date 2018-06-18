a00ae2c33f4a421f7ef03e07c6d132e43715a1e8
joepap981 <joepap981@gmail.com>
2018-06-18 17:01:54 +0900

    Restart menooing 5.0

--- js/controllers/restaurantSearchCtrl.js
+++ js/controllers/restaurantSearchCtrl.js
@@ -1,9 +1,9 @@
-angular.module('menuApp').controller('restaurantCtrl',['$scope', '$location', 'restaurantService', 'growl', '$window', function ($scope, $location, restaurantService, growl, $window) {
+angular.module('menuApp').controller('restaurantSearchCtrl',['$scope', '$location', 'restaurantService', 'growl', '$window', function ($scope, $location, restaurantService, growl, $window) {
 
   $scope.userRestaurants = [];
 
   var init = function () {
-    //get all restaurants from user in current session (check in server)
+    //get all restaurants 
     var restaurantList = restaurantService.getRestaurantList();
     restaurantList.then(function (result) {
       $scope.userRestaurants = result;
--- view/user/restaurant-search/restaurant-search-main.php
+++ view/user/restaurant-search/restaurant-search-main.php
@@ -4,6 +4,21 @@
       <p> Restaurant name </p>
     </div>
     <div class="card-body">
-      <p> This is where the restauratn information is shown </p>
+      <div id="restaurant-list" ng-controller="restaurantSearchCtrl">
+        <div class="row">
+          <div class="col-lg-4 col-sm-6 portfolio-item" ng-repeat="restaurant in userRestaurants">
+            <div class="card h-100">
+              <a href="#" ng-click="redirectToProfile(restaurant.restaurant_id)"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#" ng-click="redirectToProfile(restaurant.restaurant_id)">{{ restaurant.restaurant_name }}</a>
+                </h4>
+                <p class="card-text">{{ restaurant.restaurant_locality}}, {{ restaurant.restaurant_administrative_area_level_1 }} </p>
+                <p class="card-text"> Status: {{ restaurant.restaurant_status }} <p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
